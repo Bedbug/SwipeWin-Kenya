18820ca3094d1eab9620617b1e4489dc7ea20d4e
themisbrink <themisbrnk@gmail.com>
2021-07-01 14:29:09 +0300

    Prizes

--- src/app/prizes/prizes.component.ts
+++ src/app/prizes/prizes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { SessionService } from '../session.service';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-prizes',
@@ -12,36 +13,52 @@ export class PrizesComponent implements OnInit {
 
   public isActive: boolean = false;
   public _gamesPlayed = 0;
-  
-  constructor(private dataService : DataService, private sessionService: SessionService, private router: Router ) { }
+  isEng: boolean = true;
+
+  constructor(private dataService: DataService, private sessionService: SessionService, private router: Router, private translate: TranslateService) {
+    translate.onLangChange.subscribe(lang => {
+      if (this.translate.currentLang == 'en')
+        this.isEng = true;
+      else
+        this.isEng = false;
+
+    })
+  }
 
   ngOnInit() {
-    window.scroll(0,0);
+
+    if (this.translate.currentLang == 'en')
+      this.isEng = true;
+    else
+      this.isEng = false;
+
+
+    window.scroll(0, 0);
     if (!this.sessionService.token || !this.sessionService.isSubscribed || !this.sessionService.isEligible) {
       // wanna inform the user here?
       this.isActive = true;
-      
-    }else{
+
+    } else {
       this.isActive = false;
       this._gamesPlayed = this.sessionService.gamesPlayed;
     }
-    
-    
+
+
   }
-  
+
   public subscribe($event) {
     console.log('button is clicked');
     $event.stopPropagation();
     this.router.navigate(['home']);
   }
-  
+
   startGame() {
-      console.log("Play Main Game!");
-      this.sessionService.gamesPlayed++;
-      this.router.navigate(['game']);
-    
+    console.log("Play Main Game!");
+    this.sessionService.gamesPlayed++;
+    this.router.navigate(['game']);
+
   }
-  
+
   startFreeGame() {
     this.router.navigate(['freetimegame']);
   }
